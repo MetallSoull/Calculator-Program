21e40ab45c6aca8115a64e5bb7759363d1053b97
MetallSoull <155646500+MetallSoull@users.noreply.github.com>
2024-01-18 14:34:22 +0200

    helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -5,17 +5,21 @@ namespace numbers{
     double secondNum = 0;
     char option;
     double yourResult = 0;
+    std::string validOption;
 }
 using namespace std;
 
 int main(){
+    string ahaha = "AHAHAHAH";
     cout << "=================" << std::endl;
     cout << "   Calculator  " << std::endl;
     cout << "=================" << std::endl;
     cout << "Enter your first number: ";
     cin >> numbers::firstNum;
+    if(numbers::firstNum) {
     cout << "Enter your second number: ";
     cin >> numbers::secondNum;
+    if(numbers::secondNum) {
     cout << "Choose your option: " << std::endl;
     cout << "+ = Add" << std::endl;
     cout << "- = Substract" << std::endl;
@@ -41,12 +45,23 @@ int main(){
          numbers::yourResult = numbers::firstNum / numbers::secondNum;
          cout << "Your result equals: " << numbers::yourResult;
          break;
-        case 'Q':
+        case 'q':
          cout << "Thank you! Calculator Program by Livermar";
          break;
         default:
          cout << "You wrote the valid option!";
          break;
        }
+    } else {
+        
+        for(;;) {
+            cout << ahaha;
+        }
+    }
+    } else {
+        for(;;) {
+            cout << ahaha;
+        }
+    }
     return 0;
 }
