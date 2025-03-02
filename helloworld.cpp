f3e09a3199ffe6d0c4275cf2059f63d2880eae1f
MetallSoull <155646500+MetallSoull@users.noreply.github.com>
2025-03-02 20:22:44 +0200

    Update helloworld.cpp
    got my code updated a lil bit (basically, I just complicated it :D)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,67 +1,76 @@
 #include <iostream>
 
-namespace numbers{
-    double firstNum = 0;
-    double secondNum = 0;
-    char option;
-    double yourResult = 0;
-    std::string validOption;
-}
 using namespace std;
 
-int main(){
-    string ahaha = "AHAHAHAH";
-    cout << "=================" << std::endl;
-    cout << "   Calculator  " << std::endl;
-    cout << "=================" << std::endl;
-    cout << "Enter your first number: ";
-    cin >> numbers::firstNum;
-    if(numbers::firstNum) {
-    cout << "Enter your second number: ";
-    cin >> numbers::secondNum;
-    if(numbers::secondNum) {
-    cout << "Choose your option: " << std::endl;
-    cout << "+ = Add" << std::endl;
-    cout << "- = Substract" << std::endl;
-    cout << "* = Multiply" << std::endl;
-    cout << "/ = Divide" << std::endl;
-    cout << "Q = Close program" << endl;
-    cout << "Choose option: ";
-    cin >> numbers::option;
-    switch(numbers::option) {
-        case '+':
-         numbers::yourResult = numbers::firstNum + numbers::secondNum;
-         cout << "Your result equals: " << numbers::yourResult;
-         break;
-        case '-':
-         numbers::yourResult = numbers::firstNum - numbers::secondNum;
-         cout << "Your result equals: " << numbers::yourResult;
-         break;
-        case '*':
-         numbers::yourResult = numbers::firstNum * numbers::secondNum;
-         cout << "Your result equals: " << numbers::yourResult;
-         break;
-        case '/':
-         numbers::yourResult = numbers::firstNum / numbers::secondNum;
-         cout << "Your result equals: " << numbers::yourResult;
-         break;
-        case 'q':
-         cout << "Thank you! Calculator Program by Livermar";
-         break;
-        default:
-         cout << "You wrote the valid option!";
-         break;
-       }
-    } else {
-        
-        for(;;) {
-            cout << ahaha;
-        }
+namespace numbers {
+    double firstNumber;
+    double secondNumber;
+    string con;
+}
+
+namespace operations {
+    int choice;
+
+    int sum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
     }
-    } else {
-        for(;;) {
-            cout << ahaha;
-        }
+    int substract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
     }
+    int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+    double divide(double firstNumber, double secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
+
+using namespace operations;
+
+int main() {
+    do {
+        cout << "Calculator Program" << endl;
+        _sleep(1000);
+
+        cout << "Enter first number: " << endl;
+        cin >> ::numbers::firstNumber;
+        cout << "Enter second number: " << endl;
+        cin >> ::numbers::secondNumber;
+
+        cout << "Choose your operation: \n"
+                "1. Addition\n"
+                "2. Substraction\n"
+                "3. Multiplication\n"
+                "4. Divide\n"
+                "5. Exit\n"
+                "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+
+            case 1:
+                cout << "Result is: " << endl;
+                cout << sum(::numbers::firstNumber, ::numbers::secondNumber) << endl;
+            break;
+            case 2:
+                cout << "Result is: " << endl;
+                cout << substract(::numbers::firstNumber, ::numbers::secondNumber) << endl;
+            break;
+            case 3:
+                cout << "Result is: " << endl;
+                cout << multiply(::numbers::firstNumber, ::numbers::secondNumber) << endl;
+            break;
+            case 4:
+                cout << "Result is: " << endl;
+                cout << divide(::numbers::firstNumber, ::numbers::secondNumber) << endl;
+            break;
+            default:
+                cout << "Invalid Choice!" << endl;
+            break;
+        }
+        cout << "Would you like to continue (y/n)? " << endl;
+        cin >> ::numbers::con;
+    } while (::numbers::con=="y");
+    cout << "Bye!" << endl;
     return 0;
 }
